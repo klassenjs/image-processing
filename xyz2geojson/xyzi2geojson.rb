07a59fb17041e94d763c1c3375ba4693b22953f8
Jim Klassen <klassen.js@gmail.com>
2014-11-04 11:10:24 -0600

    Add code to convert image3 XYZRGB to other formats.
    * xyz2pcd - Binary PCD format for use with PCL.
    * xyz2geojson - GeoJSON point features for use with allover.js

---
+++ xyz2geojson/xyzi2geojson.rb
@@ -0,0 +1,83 @@
+#!/usr/bin/env ruby
+
+# Copyright (c) 2011-2013, James Klassen
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+def header()
+	print <<EOF
+{ "type": "FeatureCollection", 
+        "features": [
+EOF
+end
+	
+def feature( x,y,z,i )
+	print <<EOF
+	{ 
+                "type":"Feature",
+                "geometry": {   
+                        "type": "Point", 
+                        "coordinates": [#{x},#{y},#{z}]
+                },
+                "properties": {
+                        "X":"#{x}",
+                        "Y":"#{y}",
+                        "Z":"#{z}",
+                        "Intensity":"#{i.to_i.to_s}",
+                        "Classification":"2",
+                        "PointSourceId":"20",
+                        "Red":"#{i.to_i.to_s}",
+                        "Green":"#{i.to_i.to_s}",
+                        "Blue":"#{i.to_i.to_s}"
+                }
+        }
+EOF
+end
+
+def footer()
+	print <<EOF
+	]
+}
+EOF
+end
+
+
+def read_records() 
+	fields = nil
+	STDIN.each_line do |line|
+		line.chomp!
+
+		if(fields)
+			yield(fields)
+			print ",\n"
+		end
+	
+		fields = line.split(",")
+	end
+	yield(fields) if fields
+end
+
+
+begin
+	header
+	read_records do |fields|
+		feature(*fields)
+	end	
+	footer
+end
---
+++ xyz2pcd/xyz2pcd.cpp
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2011-2013, James Klassen
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * Change History:
+ *   10/25/2012 - modify xyz2pcd to grab color information from image3 output
+ *   10/26/2012 - reorg for one pass through input file
+ */
+
+#include <iostream>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <stdio.h>
+
+int
+  main (int argc, char** argv)
+{
+  pcl::PointCloud<pcl::PointXYZ> cloud;
+
+  // Fill in the cloud data
+  cloud.width    = 1000;
+  cloud.height   = 1;
+  cloud.is_dense = false;
+  cloud.points.resize (cloud.width * cloud.height);
+
+  FILE* xyz = fopen(argv[1], "r");
+
+  size_t i = 0;
+  float x,y,z;
+  while( !feof(xyz) )
+  {
+    fscanf( xyz, "%f %f %f\n", &x, &y, &z );
+    if( i >= cloud.width ) {
+	cloud.width += 1000;
+	cloud.points.resize (cloud.width * cloud.height);
+    }
+    cloud.points[i].x = x;
+    cloud.points[i].y = y;
+    cloud.points[i].z = z;
+
+    i++;
+  }
+  fclose(xyz);
+  cloud.width = i;
+  cloud.points.resize (cloud.width * cloud.height );
+
+  pcl::io::savePCDFileBinary ("test_pcd.pcd", cloud);
+  std::cerr << "Saved " << cloud.points.size () << " data points to test_pcd.pcd." << std::endl;
+
+  return (0);
+}
